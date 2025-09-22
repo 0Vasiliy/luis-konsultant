f50ed9e5ba0cc99b576173b1be25fb1fadea72c0
Козин-Поздняков <vasiliy_pit@mail.ru>
2025-09-22 11:17:48 +0300

    fix: correct buildAssetsDir for GitHub Pages (_nuxt under baseURL)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'static',
+    output: {
+      dir: '.output',
+      publicDir: '.output/public'
+    },
+    prerender: {
+      routes: ['/', '/qr-generator'],
+      crawlLinks: true
+    }
+  },
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/luis-konsultant/' : '/',
+    
+  },
+  ssr: false,
   devtools: { enabled: true },
   compatibilityDate: '2024-09-20',
   css: ['~/assets/scss/main.scss'],
@@ -14,5 +30,14 @@ export default defineNuxtConfig({
   },
   typescript: {
     strict: true
-  }
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000/api'
+        : '/api',
+      baseURL: process.env.NODE_ENV === 'production' ? '/luis-konsultant/' : '/',
+    }
+  },
+
 })
